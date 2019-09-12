c0f932561399e45b18ab7af10e9d5d70fe0ce1b6
Andy <>
2019-09-12 16:18:21 +0800

    拖拉並鑲嵌

--- main.js
+++ main.js
@@ -2,16 +2,35 @@
 const dragDOM = document.getElementById('drag');
 const body = document.body;
 
-const mouseDown = Rx.Observable.fromEvent(dragDOM, 'mousedown');
-const mouseUp = Rx.Observable.fromEvent(body, 'mouseup');
-const mouseMove = Rx.Observable.fromEvent(body, 'mousemove');
+const MD = Rx.Observable.fromEvent(dragDOM, 'mousedown');
+const MU = Rx.Observable.fromEvent(body, 'mouseup');
+const MM = Rx.Observable.fromEvent(body, 'mousemove');
 
-mouseDown
-	.map(event => mouseMove.takeUntil(mouseUp))
+MD
+	.map(event => MM.takeUntil(MU))
     .concatAll()
     .map(event => ({ x: event.clientX, y: event.clientY }))
     .subscribe(pos => {
-        console.log( pos.x , pos.y );
+        // console.log( pos.x , pos.y );
   	    dragDOM.style.left = pos.x + 'px';
         dragDOM.style.top = pos.y + 'px';
   })
+
+
+const videoID = 'bgi3Uaab1ok';
+const url = `'https://www.youtube.com/embed/${videoID}'`;
+
+
+$(document).ready(function(){
+    $( "#drag" ).mousedown(function(e) {
+        e.preventDefault();
+        console.log("down")
+        $('#drop').mouseup(function(){
+            $( "#drop" ).append( `<iframe width='320' height='180'src=${url} frameborder='0' id='dropIF'></iframe>`);
+            console.log(url)
+            $("drag").hide()
+         }
+        );
+      });
+});
+        
